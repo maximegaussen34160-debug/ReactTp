2e59b1369db0e70a46f9e3780ba18369806bf6cf
max34 <maximegaussen34160@gmail.com>
2026-02-19 14:49:45 +0100

    version 2

--- src/LoginFrom.jsx
+++
@@ -1,8 +0,0 @@
-function Login(email, password) {
-
-    const getUsers = () => {
-        return fetch("http://localhost:8000/api/users", {
-            type: "GET",
-        }).then((res) => res.json());
-    }
-}
